f7b96ee5a6044c70c542093f783374c20f9534f5
Randysman <134296647+Randysman@users.noreply.github.com>
2025-04-17 22:03:32 +0300

    create new app users for autorezation and switch database on postgresql

--- widnowdesigns/widnowdesigns/settings.py
+++ widnowdesigns/widnowdesigns/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'basket',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'widnowdesigns.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'widnowsdesigns',
+        'USER': 'widnowsdesigns',
+        'PASSWORD': 'widnowsdesigns',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
