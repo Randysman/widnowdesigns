ccdcb0f6507c85807a3d395e5aef2d35759f7254
Randysman <134296647+Randysman@users.noreply.github.com>
2025-04-22 16:29:59 +0300

    add forms and func order create add urls

--- widnowdesigns/orders/views.py
+++ widnowdesigns/orders/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from basket.basket import Basket
 
-# Create your views here.
+
+def order_create(request):
+    basket = Basket(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST, request=request)
+        if form.is_valid():
+            order = form.save()
+            for item in basket:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=['quantity'])
+            basket.clear()
+            return render(request, 'order/created.html', {'order': order, 'form': form})
+    else:
+        form = OrderCreateForm(request=request)
+    return render(request, 'order/created.html', {'basket': basket, 'form':form})
+
+
