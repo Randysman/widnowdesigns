3781ce42aad4b61f98a72690461b744776a324a2
Randysman <134296647+Randysman@users.noreply.github.com>
2025-04-23 19:54:18 +0300

    reg in admin page url for payment

--- widnowdesigns/orders/admin.py
+++ widnowdesigns/orders/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from  .models import Order, OrderItem
+from django.utils.safestring import mark_safe
 
 
 class OrderItemInline(admin.TabularInline):
@@ -7,8 +8,16 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_stripe_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+order_stripe_payment.short_description = 'Ссылка на оплату'
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'paid', 'created', 'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'paid', order_stripe_payment, 'created', 'updated']
     list_filter = ['paid', 'updated', 'created']
     inlines = [OrderItemInline]
