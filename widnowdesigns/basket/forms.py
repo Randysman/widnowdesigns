5d73b57909753f5ef1b676c05c1d353505eb64c1
Randysman <134296647+Randysman@users.noreply.github.com>
2025-05-12 18:46:18 +0300

    create footer

--- widnowdesigns/basket/forms.py
+++ widnowdesigns/basket/forms.py
@@ -5,5 +5,5 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 11)]
 
 
 class BasketAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int, label='Количество')
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
