13f162e7323af4400adadd60d87cdd736c503a7b
Randysman <134296647+Randysman@users.noreply.github.com>
2025-07-08 18:20:00 +0300

    add jwt-tokens

--- widnowdesigns/main/urls.py
+++ widnowdesigns/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from . import views
 from .views import ProductViewSet
@@ -16,4 +17,7 @@ urlpatterns = [
     path('shop/<slug:slug>/', views.product_detail, name='product_detail'),
     path('shop/category/<slug:category_slug>', views.product_list, name='product_list_by_category'),
     path('api/v1/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
